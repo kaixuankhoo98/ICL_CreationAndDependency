72561f2b17ee9a76e0b3032f5e7d9698d018c5c8
Kaixuan Khoo <kaixuan.khoo.98@gmail.com>
2022-02-22 15:07:25 +0000

    Created Catalogue interface and BritishLibraryCatalogue implements the interface.

--- src/main/java/ic/doc/catalogues/BritishLibraryCatalogue.java
+++ src/main/java/ic/doc/catalogues/BritishLibraryCatalogue.java
@@ -12,7 +12,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class BritishLibraryCatalogue {
+public class BritishLibraryCatalogue implements Catalogue {
 
   // imagine that each new instance of this object uses more than 500MB of RAM
   private final static BritishLibraryCatalogue instance = new BritishLibraryCatalogue();
@@ -24,6 +24,7 @@ public class BritishLibraryCatalogue {
     return instance;
   }
 
+  @Override
   public List<Book> searchFor(String query) {
     return catalogue.stream()
         .filter(book -> book.matchesAuthor(lastNameFrom(query)))
